35c6e9ac35fc70a2823ebf66967650f3d4e1a97f
Munyiwamwangi <josseymunyi@gmail.com>
2019-07-14 15:39:59 +0300

    those are the user profile deliverables

--- src/app/user.ts
+++ src/app/user.ts
@@ -1,11 +1,12 @@
-export class User {
-    constructor(public login: string,
-                // tslint:disable-next-line: variable-name
-                public avatar_url: string,
-                // tslint:disable-next-line: variable-name
-                public public_repos: string,
-                public followers: number,
-                public following: number,
-                // tslint:disable-next-line: variable-name
-                public created_at: string) { }
-}
+    export class User {
+        constructor(
+            public login: string,
+                    // tslint:disable-next-line: variable-name
+            public avatar_url: string,
+                    // tslint:disable-next-line: variable-name
+            public public_repos: string,
+            public followers: number,
+            public following: number,
+                    // tslint:disable-next-line: variable-name
+            public created_at: string) { }
+    }
