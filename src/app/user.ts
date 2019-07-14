6a3893c13e3d8f3ccf0c94b3400411ebc9fccebc
Munyiwamwangi <josseymunyi@gmail.com>
2019-07-14 15:38:41 +0300

    initialization of deliverables within the constructor, these will be passed to objects and methods assigned to fetch the deliverables from Github

--- src/app/user.ts
+++ src/app/user.ts
@@ -1,2 +1,11 @@
 export class User {
+    constructor(public login: string,
+                // tslint:disable-next-line: variable-name
+                public avatar_url: string,
+                // tslint:disable-next-line: variable-name
+                public public_repos: string,
+                public followers: number,
+                public following: number,
+                // tslint:disable-next-line: variable-name
+                public created_at: string) { }
 }
