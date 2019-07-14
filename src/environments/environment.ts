2ec8959d5a547b90a2cda126198e3c9dd73186e9
Munyiwamwangi <josseymunyi@gmail.com>
2019-07-14 16:39:39 +0300

    adding bootstrap and preparing to design and align elements

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   apiUrl: 'https://api.github.com/users/',
-  token: '?access_token=97a6dd7cdfe8358c6c2024b0b76c31f4e3271804',
+  token: '?access_token=299c55aebd62ea1f7f67c4c0dde03b840f7dad73',
 };
 
 /*
