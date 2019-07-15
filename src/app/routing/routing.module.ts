254893d2f3fdc2588f9bff3452ad87bc2eccee12
Munyiwamwangi <josseymunyi@gmail.com>
2019-07-15 10:06:06 +0300

    designing background for the repositories

--- src/app/routing/routing.module.ts
+++ src/app/routing/routing.module.ts
@@ -2,10 +2,6 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 
-const routes: Routes = [
-  // { path: 'Home', component: SearchComponent },
-  // { path: 'Search', component: NavComponent }
-];
 @NgModule({
   declarations: [],
   imports: [
